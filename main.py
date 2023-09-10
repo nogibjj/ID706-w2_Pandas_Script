27e391014007738eb91ae016991fed6b1666aee7
747hzx <59676354+0xhzx@users.noreply.github.com>
2023-09-10 16:09:47 +0000

    Iris pandas analysis

--- main.py
+++ main.py
@@ -1,13 +1,71 @@
 """
 Main cli or app entry point
 """
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 
-from mylib.calculator import add
 
-def add_two_sum(a, b):
-    add(a,b)
+def generate_iris():
+    iris = load_iris()
+    data_iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+    target = pd.DataFrame(data=iris.target, columns=["target"])
+    df = pd.concat([data_iris, target], axis=1)
+    df.to_csv("./Iris.csv", index=False)
+
+
+def load_iris_from_path(path):
+    iris_data = pd.read_csv(path)
+    return iris_data
+
+
+def desc_iris(data_iris):
+    print(data_iris.describe())
+    return data_iris.describe()
+
+
+def visual_iris(data_iris):
+    _, axs = plt.subplots(2, 2, figsize=(10, 8))
+    axs[0, 0].hist(data_iris["sepal length (cm)"], bins=10, edgecolor="black")
+    axs[0, 0].set_xlabel("Sepal Length (cm)")
+    axs[0, 0].set_ylabel("Frequency")
+    axs[0, 0].set_title("Histogram of Sepal Length")
+
+    axs[0, 1].scatter(
+        data_iris["sepal length (cm)"],
+        data_iris["sepal width (cm)"],
+        c=data_iris.target,
+        cmap="viridis",
+    )
+    axs[0, 1].set_xlabel("Sepal Length (cm)")
+    axs[0, 1].set_ylabel("Sepal Width (cm)")
+    axs[0, 1].set_title("Scatter Plot of Sepal Length vs. Sepal Width")
+
+    axs[1, 0].boxplot(
+        data_iris[data_iris.columns[:-1]].values, labels=data_iris.columns[:-1]
+    )
+    axs[1, 0].set_xlabel("Features")
+    axs[1, 0].set_ylabel("Values")
+    axs[1, 0].set_title("Box Plot of Iris Dataset")
+    axs[1, 0].set_xticklabels(data_iris.columns[:-1], rotation=45)
+
+    target_counts = data_iris["target"].value_counts()
+    axs[1, 1].pie(
+        target_counts, labels=target_counts.index, autopct="%1.1f%%", startangle=90
+    )
+    axs[1, 1].set_title("Distribution of Iris Species")
+
+    plt.tight_layout()
+    plt.savefig("iris_comparison.png")
+    plt.show()
+
+    return "iris_comparison.png"
 
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
-    print(add_two_sum(1,2))
+    # generate_iris()
+    cur_path = "./Iris.csv"
+    data = load_iris_from_path(cur_path)
+    desc_iris(data)
+    # visual_iris(data)
--- test_main.py
+++ test_main.py
@@ -3,11 +3,45 @@ Test goes here, for four functions
 
 """
 
-from mylib.calculator import *
+from main import *
 
 
-def test_all():
-    assert add(1, 2) == 3
-    assert subtract(2, 1) == 1
-    assert multiply(2,3) == 6
-    assert divide(3,2) == 1.5
+def test_load_iris_from_path():
+    path = "./Iris.csv"
+    iris_data = load_iris_from_path(path)
+    assert isinstance(iris_data, pd.DataFrame), f"load failed"
+    assert len(iris_data) == 150, f"data loaded is wrong"
+
+
+def test_desc_iris():
+    # Create a sample DataFrame for testing
+    sample_data = pd.DataFrame(
+        {
+            "sepal length (cm)": [5.1, 4.9, 4.7],
+            "sepal width (cm)": [3.5, 3.0, 3.2],
+            "petal length (cm)": [1.4, 1.3, 1.5],
+            "petal width (cm)": [0.2, 0.4, 0.3],
+            "target": [0, 1, 2],
+        }
+    )
+    description = desc_iris(sample_data)
+    assert isinstance(description, pd.DataFrame), f"data is not the format of Dataframe"
+    assert len(description) == 8, f"lost the key summary of the data"
+
+
+def test_visual_iris():
+    sample_data = pd.DataFrame(
+        {
+            "sepal length (cm)": [5.1, 4.9, 4.7],
+            "sepal width (cm)": [3.5, 3.0, 3.2],
+            "petal length (cm)": [1.4, 1.3, 1.5],
+            "petal width (cm)": [0.2, 0.4, 0.3],
+            "target": [0, 1, 2],
+        }
+    )
+    path = visual_iris(sample_data)
+    import os
+
+    # delete the file
+    os.remove(path)
+    assert path == "iris_comparison.png", f"visualization failed or file path is wrong"
